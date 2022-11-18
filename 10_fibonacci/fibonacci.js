9c60576190751520a47098bfc7ee09973e06e9dd
Arc-Data <arclight04@gmail.com>
2022-11-18 14:40:32 +0800

    Completes fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+	if(n < 0) return "OOPS";
 
+	let arr = [1, 1];
+	for(let i = 2; i <= n; i++) {
+		arr.push(arr[i - 1] + arr[i - 2]);
+	}
+	return arr[n - 1]
 };
 
 // Do not edit below this line

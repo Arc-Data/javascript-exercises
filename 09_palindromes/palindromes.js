52271c026a30b2fc236ed7a13108cda5e2d9d747
Arc-Data <arclight04@gmail.com>
2022-11-18 14:26:14 +0800

    Completes palindrome

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,13 @@
-const palindromes = function () {
+const palindromes = function (someStr) {
+	someStr = someStr.toLowerCase().replace(/[^\w]/g, '')
+	let i = 0, j = someStr.length - 1;
+	console.log(someStr)
 
+	while(i < j) {
+		if(someStr[i] != someStr[j]) return false;
+		i++, j--;
+	}
+	return true;
 };
 
 // Do not edit below this line

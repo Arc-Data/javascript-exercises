1ef66a13b5097a6cc30f30ca54514aa368b12eb9
Arc-Data <arclight04@gmail.com>
2022-11-18 15:57:23 +0800

    Completes findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+	let maxAge = 0, idx = -1;
+	people.forEach( (person, i) => {
+		let age = (person.yearOfDeath || (new Date()).getFullYear()) - person.yearOfBirth;
+		if (age > maxAge) {
+			idx = i;
+			maxAge = age;
+		}
+	})
+	return people[idx];
 };
 
 // Do not edit below this line

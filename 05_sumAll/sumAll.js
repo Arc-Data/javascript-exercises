485d5ed086f032e4828251ee171190266369f81c
Arc-Data <arclight04@gmail.com>
2022-11-18 13:11:31 +0800

    Completes sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,11 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+	if(	typeof a !== "number" || 
+		typeof b !== "number" || 
+		a < 0 || b < 0) return "ERROR";
 
+	[a, b] = a < b ? [a, b] : [b, a];
+
+	return (b - a + 1) / 2 * (a + b);
 };
 
 // Do not edit below this line
